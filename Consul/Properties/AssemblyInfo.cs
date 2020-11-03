0fc3d0986c08115709743b56963d18d1ab182462
Gautier Boder <gautier.boder@garaio.com>
2020-11-03 09:12:41 +0100

    Updated AssemblyVersion information

--- Consul/Properties/AssemblyInfo.cs
+++ Consul/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Consul.NET")]
-[assembly: AssemblyDescription(".NET API for Consul (http://www.consul.io/)")]
+[assembly: AssemblyDescription(".NET API for Consul (http://www.consul.io/) - Forked by Garaio (http://www.github.com/garaio/consuldotnet)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("G-Research Limited")]
 [assembly: AssemblyProduct("Consul.NET")]
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.1.1")]
-[assembly: AssemblyFileVersion("1.6.1.1")]
+[assembly: AssemblyVersion("1.6.1.2")]
+[assembly: AssemblyFileVersion("1.6.1.2")]
 [assembly: InternalsVisibleTo("Consul.Test, PublicKey=" +
     "002400000480000094000000060200000024000052534131000400000100010045f6337bf03a95" +
     "218f1e0b4e70bb91f8ee49fcb58593d78c1008ee646cfcf785ea60bd0b1dde6f5f92ead738e2bd" +
